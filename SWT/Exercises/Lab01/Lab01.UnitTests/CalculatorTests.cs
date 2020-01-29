ac2595675fd1d03885f186d13589e15619f45db0
nikolaj808 <45153292+nikolaj808@users.noreply.github.com>
2020-01-29 15:28:25 +0100

    Calculator commit

---
+++ SWT/Exercises/Lab01/Lab01.UnitTests/CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+using Lab01;
+
+namespace Lab01.UnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator c;
+
+        [SetUp]
+        public void SetUp()
+        {
+            c = new Calculator();
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(5, 4, 9)]
+        [TestCase(-5, 2, -3)]
+        [TestCase(9, 10, 19)]
+        public void Add_Tests_Success(double a, double b, double expected)
+        {
+            Assert.That(c.Add(a, b), Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 2, -1)]
+        [TestCase(5, 4, 1)]
+        [TestCase(-5, -5, 0)]
+        [TestCase(25, 10, 15)]
+        public void Subtract_Tests_Success(double a, double b, double expected)
+        {
+            Assert.That(c.Subtract(a, b), Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 2, 2)]
+        [TestCase(5, 4, 20)]
+        [TestCase(-5, -5, 25)]
+        [TestCase(-25, 10, -250)]
+        public void Multiply_Tests_Success(double a, double b, double expected)
+        {
+            Assert.That(c.Multiply(a, b), Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 2, 1)]
+        [TestCase(5, 5, 3125)]
+        [TestCase(2, 3, 8)]
+        [TestCase(2, 4, 16)]
+        public void Power_Tests_Success(double a, double b, double expected)
+        {
+            Assert.That(c.Power(a, b), Is.EqualTo(expected));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            c = null;
+        }
+    }
+}
---
+++ SWT/Exercises/Lab01/Lab01/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab01
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}
